3c172ac5e2b3961a04c5d344b708b3b7a653badf
mls <cjmadlife5930@gmail.com>
2025-03-12 18:49:03 +0900

    feat: 리셋기능추가

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3005;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// require('dotenv').config();
-// const cors = require('cors');
-// app.use(cors());
-
-// app.get('/', async (req, res) => {
-//   res.send('hello! aws');
-// });
-
-
-// app.listen(PORT, async () => {
-//   console.log(`server start ${PORT}`);
-// });
-
-
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3005;
 const { Counter, sequelize } = require('./model');
+const { where } = require('sequelize');
+const app = express();
+
+const PORT = process.env.PORT || 3005;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-require('dotenv').config();
-const cors = require('cors');
 app.use(cors());
 
 app.get('/', async (req, res) => {
@@ -37,30 +17,43 @@ app.get('/', async (req, res) => {
 
 app.get('/counter', async (req, res) => {
   try {
-    const result = await Counter.findOne({
+    const result = await Counter.findAll({
       order: [['id', 'DESC']],
     });
-    res.json({ value: result });
+    res.json(result);
   } catch (error) {
     console.log(error);
   }
 });
 
-app.post('/counter', async (req, res) => {
+app.post("/counter", async(req, res) => {
   try {
-    const { newValue } = req.body;
-    const newCounter = await Counter.create({ value: newValue });
-    res.json({ success: true, value: newCounter.value });
+      const { newValue } = req.body 
+      console.log(newValue);
+      if(newValue!==0){        
+        const newCounter = await Counter.create({value: newValue});
+
+        res.json(newCounter.value);
+      }else{
+  
+        await Counter.destroy({where: {}});
+        const newCounter = await Counter.create({value: 0});
+        console.log("여기오나?" ,newCounter);
+        
+        res.json(newCounter.value);
+      }
+      // const {value} = await Counter.create({value: newValue});
+     
   } catch (error) {
-    console.log(error);
+      console.log(error);
   }
-});
+})
 
 app.listen(PORT, async () => {
   await sequelize.sync({ force: true });
 
-  (await Counter.findOne({ where: { id: 1 } })) ||
-    (await Counter.create({ id: 1, value: 0 }));
+  await Counter.findOne({ where: { id: 1 } }) ||
+  await Counter.create({ id: 1, value: 0 });
 
   console.log(`server start ${PORT}`);
 });
